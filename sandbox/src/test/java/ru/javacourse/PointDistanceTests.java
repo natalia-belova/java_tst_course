6e0d5dd16c06c49abd1cde1d718fa5eb5c819eda
Natalia Belova <natalya.s.belova@gmail.com>
2022-06-29 00:44:45 +0300

    removed github testing changes

--- sandbox/src/test/java/ru/javacourse/PointDistanceTests.java
+++ sandbox/src/test/java/ru/javacourse/PointDistanceTests.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PointDistanceTests {
-    // y difference
     @Test
     public void testDistance1() {
         Point p1 = new Point(2, -3);
@@ -13,7 +12,6 @@ public class PointDistanceTests {
 
     }
 
-    //x difference
     @Test
     public void testDistance2() {
         Point p1 = new Point(4, 0);
@@ -21,15 +19,13 @@ public class PointDistanceTests {
         Assert.assertEquals(p1.distance(p2), 2);
     }
 
-    //2 diff
     @Test
     public void testDistance3() {
         Point p1 = new Point(2, 3);
         Point p2 = new Point(3, 4);
         Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
     }
-
-    //no diff
+    
     @Test
     public void testDistance4() {
         Point p1 = new Point(2, 2);
