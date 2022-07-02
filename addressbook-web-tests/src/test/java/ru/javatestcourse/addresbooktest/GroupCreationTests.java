0428c803e10f144b7498ddd7f91b0f1cf65d0067
Natalia Belova <natalya.s.belova@gmail.com>
2022-07-02 13:52:36 +0300

    moved page opening and login to setup

--- addressbook-web-tests/src/test/java/ru/javatestcourse/addresbooktest/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/javatestcourse/addresbooktest/GroupCreationTests.java
@@ -13,14 +13,14 @@ public class GroupCreationTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/addressbook/");
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
