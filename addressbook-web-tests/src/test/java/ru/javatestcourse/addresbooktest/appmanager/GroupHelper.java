cb052cf294435e756d524dfc244c6c95a114310f
Natalia Belova <natalya.s.belova@gmail.com>
2022-07-02 23:20:04 +0300

    added clear field action for consistency

--- addressbook-web-tests/src/test/java/ru/javatestcourse/addresbooktest/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/javatestcourse/addresbooktest/appmanager/GroupHelper.java
@@ -12,7 +12,7 @@ public class GroupHelper {
     }
 
     public void returnToGroupsPage() {
-      wd.findElement(By.linkText("group page")).click();
+        wd.findElement(By.linkText("group page")).click();
     }
 
     public void clickDeleteGroup() {
@@ -20,20 +20,23 @@ public class GroupHelper {
     }
 
     public void submitGroupForm() {
-      wd.findElement(By.name("submit")).click();
+        wd.findElement(By.name("submit")).click();
     }
 
     public void enterGroupFormValues(GroupObject groupObject) {
-      wd.findElement(By.name("group_name")).click();
-      wd.findElement(By.name("group_name")).sendKeys(groupObject.getName());
-      wd.findElement(By.name("group_header")).click();
-      wd.findElement(By.name("group_header")).sendKeys(groupObject.getHeader());
-      wd.findElement(By.name("group_footer")).click();
-      wd.findElement(By.name("group_footer")).sendKeys(groupObject.getFooter());
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupObject.getName());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupObject.getHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupObject.getFooter());
     }
 
     public void initiateNewGroup() {
-      wd.findElement(By.name("new")).click();
+        wd.findElement(By.name("new")).click();
     }
 
     public void selectGroupByOrder(String groupOrder) {
