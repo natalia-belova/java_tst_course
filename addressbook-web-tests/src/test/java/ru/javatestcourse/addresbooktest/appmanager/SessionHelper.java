26921acb3707063f6861bb41f3a926e978c518ce
Natalia Belova <natalya.s.belova@gmail.com>
2022-07-02 23:23:46 +0300

    added click and clear field actions for consistency

--- addressbook-web-tests/src/test/java/ru/javatestcourse/addresbooktest/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/javatestcourse/addresbooktest/appmanager/SessionHelper.java
@@ -11,7 +11,11 @@ public class SessionHelper {
     }
 
     public void login(String username, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
