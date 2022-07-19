bfd857910379fb6e8a04836e649885be35321879
Natalia Belova <natalya.s.belova@gmail.com>
2022-07-19 11:34:47 +0300

    play with cycles - execution time

--- sandbox/src/main/java/ru/javacourse/Primes.java
+++ sandbox/src/main/java/ru/javacourse/Primes.java
@@ -10,6 +10,34 @@ public class Primes {
         return true;
     }
 
+    public static boolean isPrimeFast(int n) {
+        for (int i = 2; i < n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPrimeFastest(int n) {
+        int m = (int) Math.sqrt(n);
+        for (int i = 2; i < m; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPrime(long n) {
+        for (long i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static boolean isPrimeWhile(int n) {
         int i = 2;
         while (i < n && n % i != 0) {
--- sandbox/src/test/java/ru/javacourse/PrimeTests.java
+++ sandbox/src/test/java/ru/javacourse/PrimeTests.java
@@ -4,33 +4,53 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PrimeTests {
-    @Test
+    @Test(enabled = false)
+//    3 - простое число
     public void testPrime1() {
         Assert.assertTrue(Primes.isPrime(3));
     }
 
-    @Test
+    @Test(enabled = false)
     public void testPrime2() {
         Assert.assertFalse(Primes.isPrime(4));
     }
 
     @Test
+//    2147483647 - простое число
     public void testPrime3() {
         Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
     }
 
-        @Test
-        public void testPrimeW1() {
-            Assert.assertTrue(Primes.isPrimeWhile(3));
-        }
+    @Test(enabled = false)
+//    performance of such test is several times lower coz of 'long' data type
+    public void testPrimeLong() {
+        long n = Integer.MAX_VALUE;
+        Assert.assertTrue(Primes.isPrime(n));
+    }
 
-        @Test
-        public void testPrimeW2() {
-            Assert.assertFalse(Primes.isPrimeWhile(4));
-        }
+    @Test(enabled = false)
+    public void testPrimeW1() {
+        Assert.assertTrue(Primes.isPrimeWhile(3));
+    }
 
-        @Test
-        public void testPrimeW3() {
-            Assert.assertTrue(Primes.isPrimeWhile(Integer.MAX_VALUE));
-        }
+    @Test(enabled = false)
+    public void testPrimeW2() {
+        Assert.assertFalse(Primes.isPrimeWhile(4));
+    }
+
+    @Test
+    public void testPrimeW3() {
+        Assert.assertTrue(Primes.isPrimeWhile(Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void testPrimeFast() {
+        Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void testPrimeFastest() {
+        Assert.assertTrue(Primes.isPrimeFastest(Integer.MAX_VALUE));
+    }
+
 }
