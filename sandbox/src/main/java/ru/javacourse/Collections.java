4936516c8000dee150c3fdece2cf5f491c092394
Natalia Belova <natalya.s.belova@gmail.com>
2022-07-19 11:50:25 +0300

    collections 1

---
+++ sandbox/src/main/java/ru/javacourse/Collections.java
@@ -0,0 +1,17 @@
+package ru.javacourse;
+
+public class Collections {
+//    String[] args - массив
+    public static void main(String[] args) {
+        String[] langs = {"Java", "Python", "Go", "JS"};
+        for (int i = 0; i < langs.length; i++) {
+            System.out.println(langs[i]);
+        }
+//        String[] langs = {"Java", "Python", "Go", "JS"}; - это коротка форма для
+//        String[] langs = new String[4];
+//        langs[0] = "Java";
+//        langs[1] = "Python";
+//        langs[2] = "Go";
+//        langs[3] = "JS";
+    }
+}
