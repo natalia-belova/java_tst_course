acc3aebd9f3259c09867ec31106d9fc59fcb681f
Natalia Belova <natalya.s.belova@gmail.com>
2022-07-19 12:12:24 +0300

    collections 3

--- sandbox/src/main/java/ru/javacourse/Collections.java
+++ sandbox/src/main/java/ru/javacourse/Collections.java
@@ -1,10 +1,21 @@
 package ru.javacourse;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
 //    String[] args - массив
     public static void main(String[] args) {
+//        массив
         String[] langs = {"Java", "Python", "Go", "JS"};
-        for (String l : langs) {
+//        список
+        List<String> languages = new ArrayList<String>();
+        languages.add("Java");
+        languages.add("Python");
+//        короткая запись для списка
+        List<String> languages2 = Arrays.asList("Java", "Python", "Go", "JS");
+        for (String l : languages2) {
             System.out.println(l);
         }
     }
