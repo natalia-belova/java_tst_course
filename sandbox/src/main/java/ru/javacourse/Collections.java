faabaf394afc810fb2d0d414ea592bcb8ee4abb2
Natalia Belova <natalya.s.belova@gmail.com>
2022-07-19 11:52:06 +0300

    collections 2

--- sandbox/src/main/java/ru/javacourse/Collections.java
+++ sandbox/src/main/java/ru/javacourse/Collections.java
@@ -4,14 +4,8 @@ public class Collections {
 //    String[] args - массив
     public static void main(String[] args) {
         String[] langs = {"Java", "Python", "Go", "JS"};
-        for (int i = 0; i < langs.length; i++) {
-            System.out.println(langs[i]);
+        for (String l : langs) {
+            System.out.println(l);
         }
-//        String[] langs = {"Java", "Python", "Go", "JS"}; - это коротка форма для
-//        String[] langs = new String[4];
-//        langs[0] = "Java";
-//        langs[1] = "Python";
-//        langs[2] = "Go";
-//        langs[3] = "JS";
     }
 }
